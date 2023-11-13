bcce1412be5933c88ce2ec4b079702cb2ac6bc37
riccardotessarin <riccardotex@gmail.com>
2023-11-13 18:48:56 +0100

    Working with HD player and NPC

---
+++ Assets/Prefabs/Player/RPG Cameras & Controllers/Scripts/Inputs/RPGInputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace JohnStairs.RCC.Inputs {
+    public class RPGInputManager {
+        private static RPGInputActions _inputActions;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init() {
+            _inputActions = null;
+        }
+
+        public static RPGInputActions GetInputActions() {
+            if (_inputActions == null) {
+                _inputActions = new RPGInputActions();
+                _inputActions.Enable();
+            }
+
+            return _inputActions;
+        }
+
+        public static void DisableInputActions() {
+            if (_inputActions != null) {
+                _inputActions.Disable();
+            }
+        }
+
+        public static void EnableInputActions() {
+            if (_inputActions != null) {
+                _inputActions.Enable();
+            }
+        }
+    }
+}
